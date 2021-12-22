dd1061c01e3501f863864cdc45e1ca344f4e5301
Soffeine <Soffein@yandex.ru>
2021-12-22 16:35:32 +0300

    working with cookie

--- controllers/signin.js
+++ controllers/signin.js
@@ -32,7 +32,8 @@ const login = (req, res, next) => {
       res.cookie('jwt', token, {
         maxAge: 3600000 * 24 * 7,
         httpOnly: true,
-
+        sameSite: 'none',
+        secure: true,
       }).send({
         name: user.name,
         email: user.email,
